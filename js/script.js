679e89728883157ee91515d5cd44f855c1688273
salvatorecoppola <102369392+salvatorecoppola@users.noreply.github.com>
2022-11-14 14:52:44 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,69 +1,67 @@
 
-//adding div for result
-var numb = document.createElement('div');
-numb.classList = ("numb");
-document.body.append(numb);
+//starting with add div for result
+var displayNumberDiv = document.createElement('div');
+displayNumberDiv.classList = ("numb");
+document.body.append(displayNumberDiv);
 
 
 //adding <p> in div for result
 var p = document.createElement('p');
 p.classList = ("p")
 p.textContent = 0
-numb.appendChild(p);
+displayNumberDiv.appendChild(p);
 
+
+// initializing the variable with zero which we will refer to the functions to add or subtract the value 
 let counter = 0;
 
 
-//adding a div for the button
-let container = document.createElement('div');
-container.classList = ("container");
-document.body.appendChild(container);
+//adding a container div for the buttons
+let buttonsContainer = document.createElement('div');
+buttonsContainer.classList = ("container");
+document.body.appendChild(buttonsContainer);
 
 
-//adding increment button and appending in 'numb' div
-var btn = document.createElement('button');
-btn.classList = ("increase");
-btn.textContent = "+";
-container.appendChild(btn);
+//writing a function that helps me create a button whenever I need it
 
-//increment button function
-btn.setAttribute('data-counter', counter);
-btn.addEventListener("click",()=>{
-    counter++;
-    btn.setAttribute('data-counter', counter);
-    p.innerHTML = counter ;
-});
+function createButton (type, classList, text) {
+  var btn = document.createElement('button');
+  btn.setAttribute('value', type)
+  btn.innerText = (text)
+  btn.classList.add(classList);
+  buttonsContainer.appendChild(btn);
+};
 
-
-
-
-//adding decrease button and appending in 'numb' div
-var btn2 = document.createElement('button');
-btn2.classList = ("decrease");
-btn2.textContent = "-";
-container.appendChild(btn2);
-
-//decrease button function
-btn2.setAttribute('data-counter2', counter);
-btn2.addEventListener("click",()=>{
-    counter--;
-    
-    btn2.setAttribute('data-counter2',counter);
-    p.innerText = counter ;
-});
+createButton ('increaseByOneButton','increaseByOneButton', '+'); //adding increment button and appending in 'container' div
+createButton ('decreaseByOneButton','decreaseByOneButton','-'); //adding decrease button and appending in 'container' div
+createButton ( 'plusTenButton','plusTenButton','+10');//adding plusTen button and appending in 'container' div
+createButton ( 'minusTenButton','minusTenButton','-10');//adding minusTen button and appending in 'container' div
+createButton ( 'resetButton','resetButton', 'reset'); //adding reset button and appending in 'container' div
 
 
-//adding reset button and appending in 'numb' div
-var reset = document.createElement('button');
-reset.classList = ("reset");
-reset.textContent = "reset";
-container.appendChild(reset);
+// creating an event listener function that on click, through event target class name,
+// will look for the button with the same class and automatically add its own function
 
-//reset button function
-reset.setAttribute('reset-counter', counter);
-reset.addEventListener("click",()=>{
-    counter = 0;
-    reset.setAttribute('reset-counter', counter);
-    p.innerHTML = counter ;
+buttonsContainer.addEventListener("click", (event) => {
+  //increment by one function
+  if (event.target.className === 'increaseByOneButton') {
+    counter++
+    p.innerText = counter; return
+  //increment by ten function
+  } else if (event.target.className === 'plusTenButton') {
+    counter+=10
+    p.innerText = counter; return
+  //decrement by one function
+  } else if (event.target.className === 'decreaseByOneButton') 
+    { counter--
+    p.innerText = counter; return
+  //decrement by ten function
+  } else if (event.target.className === 'minusTenButton') 
+    { counter-=10
+    p.innerText = counter; return
+  //reset function 
+  } else ( event.target.className === 'resetButton') 
+    counter = 0
+    p.innerText = counter; return
 });
 
